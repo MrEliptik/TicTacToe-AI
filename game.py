fde0a7b1af84a7e3f75a84243638902a5bbf0bab
MrEliptik <victormeunier.dev@gmail.com>
2018-12-07 16:57:29 +0200

    Added game logic

--- game.py
+++ game.py
@@ -1,20 +1,23 @@
 import numpy as np
 import random
+import re
 
 class Grid():
     def __init__(self):
         self.grid = np.full((3,3), None)
 
+    def update(self, x, y, symbol):
+        if(self.grid[x][y] == None):
+            self.grid[x][y] = symbol
+            return True
+        else:
+            print("Cell already used!")
+            return False
+
     def __str__(self):
-        grid = "["
+        grid = ""
         for i, cell in enumerate(self.grid):
-            if(i == 2 or i == 5):
-                grid += " " + str(cell) + " ]\n"
-            if(i == 3 or i == 6):
-                grid += "[ " + str(cell)
-            else:
-                grid += " " + str(cell)
-        grid += " ]" 
+            grid += " " + str(cell) + "\n"
         return grid
 
 class Player():
@@ -24,13 +27,82 @@ class Player():
         self.won_games = 0
         self.draw_games = 0
 
+def alignement(grid):
+    if(    grid[0][0]==grid[0][1]==grid[0][2] and grid[0][0] != None #vertical
+        or grid[1][0]==grid[1][1]==grid[1][2] and grid[0][0] != None #vertical
+        or grid[2][0]==grid[2][1]==grid[2][2] and grid[0][0] != None #vertical
+
+        or grid[0][0]==grid[1][0]==grid[2][0] and grid[0][0] != None #horizontal
+        or grid[0][1]==grid[1][1]==grid[2][1] and grid[0][0] != None #horizontal
+        or grid[0][2]==grid[1][2]==grid[2][2] and grid[0][0] != None #horizontal
+
+        or grid[0][0]==grid[1][1]==grid[2][2] and grid[0][0] != None #diagonal
+        or grid[0][1]==grid[1][2]==grid[3][1] and grid[0][0] != None #diagonal
+    ): 
+        return True
+    else:
+        return False
+
+def gridFull(grid):
+    for rows in grid:
+        for cell in rows:
+            if cell == None:
+                return False
+    return True
+
 def gameLoop(p1, p2):
+
+    def getPlayerInput():
+        print("Enter next move : x y (0 < x y < 2)")
+        x = '-1'
+        y = '-1'
+        while((int(x) < 0 or int(x) > 2) or (int(y) < 0 or int(y) > 2)):
+            location = input()
+            x, y = re.split(' ', location)
+            if(int(x) >= 0 and int(x) < 3 and int(y) >= 0 and int(y) < 3):
+                return int(x), int(y)
+            else:
+                print("Location not valid!\n Must be : x y with 0 < x y < 2")
+
+    def switchPlayer(turn):
+        if(turn == p1):
+            return p2
+        else:
+            return p1
 
     # Initiliaze the Grid
     grid = Grid()
 
     # Choose randomly a player
-    playerTurn = random.randint(1, 2)
+    if(random.randint(1, 2) == 1):
+        playerTurn = p1
+    else:
+        playerTurn = p2
+    
+    # Display the grid
+    print(grid)
+
+    # Player place its symbol
+    x, y = getPlayerInput()
+    while(not grid.update(x, y, playerTurn.symbole)):
+        x, y = getPlayerInput()
+    
+    print(grid)
+
+    while(not alignement(grid.grid) and not gridFull(grid.grid)):
+        # Switch player
+        playerTurn = switchPlayer(playerTurn)
+
+        # Get player input
+        x, y = getPlayerInput()
+        while(not grid.update(x, y, playerTurn.symbole)):
+            x, y = getPlayerInput()
+
+        # Display the grid
+        print(grid)
+
+            
+    
 
 
 if __name__ == "__main__":
