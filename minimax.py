13d6788ef5980d65d4c2e39f1677525cf33663fe
Victor <mreliptik@ubuntu16.04>
2019-03-12 18:44:09 +0100

    refactor

--- minimax.py
+++ minimax.py
@@ -9,8 +9,7 @@ def minimax(state, depth, player):
             return 10, state
         elif winner == "X":
             return -10, state
-        else:
-            return 0, state
+        return 0, state
     if game.gridFull(state):
         return 0, state
 
@@ -26,15 +25,14 @@ def minimax(state, depth, player):
                 best_cell = cell
                 best = v
         return best, best_cell
-    else:
-        best = +infinity
-        best_cell = None
-        games = game.empty_cells(state)
-        for cell in games:
-            state[cell[0]][cell[1]] = player
-            v, move = minimax(state, depth-1, "O")
-            state[cell[0]][cell[1]] = None
-            if v <= best:
-                best_cell = cell
-                best = v
-        return best, best_cell
+    best = +infinity
+    best_cell = None
+    games = game.empty_cells(state)
+    for cell in games:
+        state[cell[0]][cell[1]] = player
+        v, move = minimax(state, depth-1, "O")
+        state[cell[0]][cell[1]] = None
+        if v <= best:
+            best_cell = cell
+            best = v
+    return best, best_cell
